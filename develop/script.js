b91c773ec27d760d42a3edb5c974be7f41912b4e
lord vader <dillon.roth11@gmail.com>
2022-01-10 08:33:41 -0800

    started adding window prompts after button is clicked

--- develop/script.js
+++ develop/script.js
@@ -25,8 +25,6 @@ var parameters = {
     symbol: symAsk
 };
 
-console.log(parameters.length);
-
 var lowerAsk = window.confirm("Would you like lowercase letters in your password?");
 console.log(lowerAsk);
 
@@ -67,10 +65,25 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    window.alert("button was clicked");
+    var lowerAsk = window.confirm("Would you like lowercase letters in your password?");
+    console.log(lowerAsk);
 
-  passwordText.value = password;
+    var upperAsk = window.confirm("Would you like uppercase letters in your password?");
+    console.log(upperAsk);
+
+    var numAsk = window.confirm("Would you like numbers in your password?");
+    console.log(numAsk);
+
+    var symAsk = window.confirm("would you like special characters in your password?");
+    console.log(symAsk);
+
+    console.log(parameters.lower);
+
+    //var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
 
 }
 
